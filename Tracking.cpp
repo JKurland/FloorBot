7881cf54361bf21712e0331093587a44985406dd
goatlover <joshua.kurland@outlook.com>
2015-02-06 08:14:47 +0000

    Position Updating added

--- Tracking.cpp
+++ Tracking.cpp
@@ -1,4 +1,5 @@
 #include "Tracking.h"
+#include <time.h>
 using namespace std;
 
 
@@ -9,6 +10,8 @@ void Update(Robot* Hasard){
 	if (Hasard->tach[0].side){ //0 is left 1 is right (wrt tach.side)
 		swap(Ldist, Rdist);
 	}
-	Hasard->pos.ang = oldang + (Rdist - Ldist) / Hasard->width; //width is measured from the centre of each wheel
-
+	float newang = oldang + (Rdist - Ldist) / Hasard->width; //width is measured from the centre of each wheel
+	Hasard->pos.x += (sin(newang) - sin(oldang))*Hasard->width*(Ldist + Rdist) / (2 * (Rdist - Ldist));
+	Hasard->pos.y += (cos(oldang) - cos(newang))*Hasard->width*(Ldist + Rdist) / (2 * (Rdist - Ldist));
+	Hasard->pos.ang = newang;
 };
