07b46592f559026debb4555efccdcb8ad80cd6fb
goatlover <joshua.kurland@outlook.com>
2015-02-06 15:11:04 +0000

    Added Basic Classes

--- Map.h
+++ Map.h
@@ -3,6 +3,27 @@
 
 #include "Robot.h"
 
+class Point{
+  float x;
+  float y;
+  float ang(float x, float y); //angle to a Point from another point realtive to the x axis
+}
+
+class Wall{
+  vector<Point> points;
+  float maxang(Position); //maximum ang of all the points in wall
+  float minang(Position);
+  Wall(Point p[]);
+  Wall();
+}
+
+class Map{
+  vector<Wall> walls;
+  Map(Wall w[]);
+  Map();
+  float LoS(Position); //length of the line of sight from a position at an angle
+}
+
 void UpdateMap(Robot*);
 
 #endif
