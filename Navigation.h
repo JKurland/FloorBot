c55f0d58e01f3aee6f5e1a4e2eee4027d2e4640f
goatlover <joshua.kurland@outlook.com>
2015-02-06 10:27:05 +0000

    Command class now controls wheels directly

--- Navigation.h
+++ Navigation.h
@@ -10,13 +10,14 @@ class Command{
 public:
 	int left;
 	int right;
+	bool complete;
 	Command(float, float);
 	Command();
 };
 
 
 
-Command Navigate(Position* Target, Robot* Hasard);
+Command Navigate(Position* Target, Robot* Hasard, float margin);
 float Pos2Ang(Position* Target, Position* Origin);
 float Distance(Position* point1, Position* point2);
 #endif
